cffb5716d4aed12698ae7f700f88dbd5f179faf8
Yogesh Nagre <119948989+Yogeshnagre00@users.noreply.github.com>
2023-12-28 16:42:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
 //your JS code here. If required.
-const commentsContianer=document.querySelector(".comments-container");
-const textBoxInput=document.querySelector("#textInput");
-const submitBtn=document.querySelector("#submit-btn");
-
-submitBtn.addEventListener("click",()=>{
-	if(textBoxInput.value.length!==0){
-		const div=document.createElement("div");
-		div.classList.add("comment")
-		const date=new Date().toUTCString();
-		div.innerHTML=`
-						<h1>${textBoxInput.value}</h1>
-							<div class="author">
-								<p>Piyush</p>
-								<p>${date}</p>
-							</div>
-  `
-		commentsContianer.appendChild(div);
-	}
-})
+const date = document.querySelector('.date')
+const d = new Date()
+date.innerText = d
